f9da0bc1ed79840ef2f7f2676cec70def4095115
Chelsea <chelseasingla123@gmail.com>
2024-03-14 00:43:01 +0530

    new error update

--- Program.cs
+++ Program.cs
@@ -14,7 +14,7 @@ namespace WebApplication4
             // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
             {
-                app.UseExceptionHandler("/Errornew");
+                app.UseExceptionHandler("/Error new ");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
